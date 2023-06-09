08cb89092b317aeca96e5cdf9c351e3ae7183ff6
Patrick Wieth <patrick.wieth@googlemail.com>
2023-06-09 14:53:08 +0200

    Revert "fix uptick 3 image formats"
    This reverts commit de6da41e964e428a705de6bfd8330359d9d01af1.

--- game-server.js
+++ game-server.js
@@ -15,6 +15,8 @@ game.addChain("Stargaze")
 game.addChain("Omniflix")
 game.addChain("Uptick")
 
+
+/*
 const cert = fs.readFileSync('/etc/letsencrypt/live/nftarena.cc/cert.pem');
 const ca = fs.readFileSync('/etc/letsencrypt/live/nftarena.cc/chain.pem');
 const key = fs.readFileSync('/etc/letsencrypt/live/nftarena.cc/privkey.pem');
@@ -22,6 +24,7 @@ var options = {
   key: key,
   cert: cert
 };
+*/ var options = {}
 
 var app = express()
 
